92cf6f2e4f19dc563a642cb66b4e64f5582e58a9
stefano <sirfoga@protonmail.com>
2016-11-13 17:10:51 +0100

    released crawler: use at your risk

---
+++ README.md
@@ -0,0 +1,10 @@
+# esf-bot: esf data crawler from Formula Student Germany website.
+
+## Aim
+* download entire esf module from Formula Student Germany website.
+
+## Solution
+* simple, ready to use script
+
+## LICENSE
+[Apache License](http://www.apache.org/licenses/LICENSE-2.0) Version 2.0, January 2004
---
+++ src/merge_csv.py
@@ -0,0 +1,19 @@
+import os
+from hal.files import models
+from hal.files import manager
+
+
+DIRECTORY = "/home/stefano/Coding/Python/misc/misc/web/fsgermany/esf/"
+out_filename = os.path.join(DIRECTORY, "esf.csv")
+out_data = ""
+files = models.Directory().ls(DIRECTORY, False)  # get list of files
+for f in files:
+	directory, name = manager.extract_path_name(f)
+	name = name.replace(".csv", "")
+	data = open(f).read()
+	print(name)
+	out_data += "\"" + str(name) + "\"" + "\n"  # .csv
+	out_data += data + "\n\n"
+
+with open(out_filename, "w") as o:
+	o.write(out_data)
